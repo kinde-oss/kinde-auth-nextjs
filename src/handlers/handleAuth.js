67986ac37415328c3baec4d87bbd7e1b165a4ff8
Peter Phanouvong <peterphanouvong@gmail.com>
2022-05-19 13:13:51 +1000

    return function

--- src/handlers/handleAuth.js
+++ src/handlers/handleAuth.js
@@ -1,14 +1,15 @@
 import { login } from "./login";
 
-export default function handler(req, res) {
-  let {
-    query: { kindeAuth: route },
-  } = req;
+export default () =>
+  function handler(req, res) {
+    let {
+      query: { kindeAuth: route },
+    } = req;
 
-  switch (route) {
-    case "login":
-      return login(req, res);
-    default:
-      res.status(404).end();
-  }
-}
+    switch (route) {
+      case "login":
+        return login(req, res);
+      default:
+        res.status(404).end();
+    }
+  };
