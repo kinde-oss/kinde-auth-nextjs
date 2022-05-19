b5833ba6ead9fbe9ca34ded8822365659f17bd4f
Peter Phanouvong <peterphanouvong@gmail.com>
2022-05-19 13:29:12 +1000

    fix route

--- src/handlers/handleAuth.js
+++ src/handlers/handleAuth.js
@@ -2,14 +2,12 @@ import { login } from "./login";
 
 export default () =>
   function handler(req, res) {
-    console.log(req);
-    console.log("req.query", req.query);
-
     let {
       query: { kindeAuth: route },
     } = req;
 
-    console.log("route", route);
+    route = Array.isArray(route) ? route[0] : route;
+
     switch (route) {
       case "login":
         login(req, res);
