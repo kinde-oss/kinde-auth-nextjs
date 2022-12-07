6e2f0bdf5e668dd31e66af14b86870ecc02700c6
Peter Phanouvong <peterphanouvong@gmail.com>
2022-12-08 10:00:45 +1100

    new functions

---
+++ src/session/getClaim.js
@@ -0,0 +1,12 @@
+import { getAccessToken } from "./getAccessToken";
+import { getIdToken } from "./getIdToken";
+
+export const getClaim = (req, res, claim) => {
+  const accessToken = getAccessToken(req, res);
+  return accessToken ? accessToken[claim] : null;
+};
+
+export const getClaimFromIdToken = (req, res, claim) => {
+  const idToken = getIdToken(req, res);
+  return idToken ? idToken[claim] : null;
+};
