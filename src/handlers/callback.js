50b68e41bf3f4e71492dffb0a81de89164f8e124
Tom West <55838419+twestos@users.noreply.github.com>
2023-04-14 15:20:36 +1000

    added post login redirect url

--- src/handlers/callback.js
+++ src/handlers/callback.js
@@ -3,64 +3,59 @@ import { config } from "../config/index";
 var cookie = require("cookie");
 
 export const callback = async (req, res) => {
-  const { code, state } = req.query;
-  const code_verifier = cookie.parse(req.headers.cookie || "")[
-    `${config.SESSION_PREFIX}-${state}`
-  ];
+	const { code, state } = req.query;
+	const code_verifier = cookie.parse(req.headers.cookie || "")[`${config.SESSION_PREFIX}-${state}`];
 
-  if (code_verifier) {
-    try {
-      const response = await fetch(
-        config.issuerURL + config.issuerRoutes.token,
-        {
-          method: "POST",
-          headers: new Headers({
-            "Content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-          }),
-          body: new URLSearchParams({
-            client_id: config.clientID,
-            client_secret: config.clientSecret,
-            code,
-            code_verifier,
-            grant_type: "authorization_code",
-            redirect_uri: config.redirectURL + config.redirectRoutes.callback,
-          }),
-        }
-      );
-      const data = await response.json();
-      const accessTokenHeader = jwt_decode(data.access_token, { header: true });
-      const accessTokenPayload = jwt_decode(data.access_token);
+	if (code_verifier) {
+		try {
+			const response = await fetch(config.issuerURL + config.issuerRoutes.token, {
+				method: "POST",
+				headers: new Headers({
+					"Content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+				}),
+				body: new URLSearchParams({
+					client_id: config.clientID,
+					client_secret: config.clientSecret,
+					code,
+					code_verifier,
+					grant_type: "authorization_code",
+					redirect_uri: config.redirectURL + config.redirectRoutes.callback,
+				}),
+			});
+			const data = await response.json();
+			const accessTokenHeader = jwt_decode(data.access_token, { header: true });
+			const accessTokenPayload = jwt_decode(data.access_token);
 
-      let isAudienceValid = true;
-      if (config.audience)
-        isAudienceValid = accessTokenPayload.aud == config.audience;
+			let isAudienceValid = true;
+			if (config.audience) isAudienceValid = accessTokenPayload.aud == config.audience;
 
-      if (
-        accessTokenPayload.iss == config.issuerURL &&
-        accessTokenHeader.alg == "RS256" &&
-        accessTokenPayload.exp > Math.floor(Date.now() / 1000) &&
-        isAudienceValid
-      ) {
-        res.setHeader(
-          "Set-Cookie",
-          cookie.serialize(`kinde_token`, JSON.stringify(data), {
-            httpOnly: true,
-            expires: new Date(accessTokenPayload.exp * 1000),
-            sameSite: "strict",
-            secure: true,
-            path: "/",
-          })
-        );
-      } else {
-        console.error("One or more of the claims were not verified.");
-      }
-    } catch (err) {
-      console.error(err);
-    }
-    res.redirect(config.redirectURL);
-  } else {
-    const logoutURL = new URL(config.issuerURL + config.issuerRoutes.logout);
-    logoutURL.searchParams.set("redirect", config.postLogoutRedirectURL);
-    res.redirect(logoutURL.href);
-  }
+			if (
+				accessTokenPayload.iss == config.issuerURL &&
+				accessTokenHeader.alg == "RS256" &&
+				accessTokenPayload.exp > Math.floor(Date.now() / 1000) &&
+				isAudienceValid
+			) {
+				res.setHeader(
+					"Set-Cookie",
+					cookie.serialize(`kinde_token`, JSON.stringify(data), {
+						httpOnly: true,
+						expires: new Date(accessTokenPayload.exp * 1000),
+						sameSite: "strict",
+						secure: true,
+						path: "/",
+					})
+				);
+			} else {
+				console.error("One or more of the claims were not verified.");
+			}
+		} catch (err) {
+			console.error(err);
+		}
+		const redirectUrl = config.postLoginURL ? config.postLoginURL : config.redirectURL;
+		res.redirect(redirectUrl);
+	} else {
+		const logoutURL = new URL(config.issuerURL + config.issuerRoutes.logout);
+		logoutURL.searchParams.set("redirect", config.postLogoutRedirectURL);
+		res.redirect(logoutURL.href);
+	}
 };
