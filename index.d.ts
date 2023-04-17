e0de31b4fb8fefc3bf1458365cfecb9657a74632
Dave Berner <davidajberner@gmail.com>
2023-04-17 11:27:34 +1000

    make bundle dist consistent with other SDKs

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,15 @@
 import React from "react";
 
 export declare function useKindeAuth(): State;
+
 export declare function KindeProvider({
   children,
 }: {
   children: any;
 }): React.Provider<State>;
+
 export declare function handleAuth(): any;
+
 export type User = {
   id: string;
   name: string | null;
@@ -15,6 +18,7 @@ export type User = {
   family_name: string | null;
   updated_at: string | null;
 };
+
 export type State = {
   user: User;
   isLoading: boolean;
