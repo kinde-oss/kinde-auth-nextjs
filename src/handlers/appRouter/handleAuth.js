8360ed4c66533b669fbe56f18a189f8dd4960a64
Dave Berner <davidajberner@gmail.com>
2023-06-20 13:53:23 +1000

    feat: create org endpoint

--- src/handlers/appRouter/handleAuth.js
+++ src/handlers/appRouter/handleAuth.js
@@ -3,8 +3,10 @@ import {login} from './login';
 import {logout} from './logout';
 import {register} from './register';
 import {callback} from './callback';
+import {createOrg} from './createOrg';
 
 const routeMap = {
+  create_org: createOrg,
   register,
   login,
   logout,
@@ -18,5 +20,7 @@ const getRoute = (endpoint) => {
 export async function handleAuth(request, endpoint) {
   const route = getRoute(endpoint);
 
-  return route ? await route(request) : 'nope';
+  return route
+    ? await route(request)
+    : new Response('This page could not be found.', {status: 404});
 }
