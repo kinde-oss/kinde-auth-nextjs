00124893040dc871bb292f2763d76a6741fde146
Dave Berner <davidajberner@gmail.com>
2023-08-31 10:19:56 +1000

    feat: isTokenValid helper for pages router

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export {KindeProvider, useKindeAuth} from './frontend/index';
 export {handleAuth} from './handlers/pageRouter/index';
+export {isTokenValid} from './utils/pageRouter/isTokenValid';
 export * from './session/pagesRouter/index';
