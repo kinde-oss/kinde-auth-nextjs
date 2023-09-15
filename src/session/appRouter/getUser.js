1666f726e2bd548ba246a4e7ddca8d6d2f1eca6a
Peter Phanouvong <peterphanouvong@gmail.com>
2023-09-15 10:50:26 +1000

    remove logs

--- src/session/appRouter/getUser.js
+++ src/session/appRouter/getUser.js
@@ -5,10 +5,8 @@ import {kindeClient} from './kindeServerClient';
 const getUser = async (req, res) => {
   try {
     const user = await kindeClient.getUser(sessionManager(cookies()));
-    console.log('user', user);
     return user;
   } catch (error) {
-    console.log(error);
     return null;
   }
 };
