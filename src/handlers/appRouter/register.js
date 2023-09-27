ee01c2ce686f42ff1199c64ff319e2ce8d5db44f
Peter Phanouvong <peterphanouvong@gmail.com>
2023-09-27 13:55:33 +1000

    post login redirect for app router

--- src/handlers/appRouter/register.js
+++ src/handlers/appRouter/register.js
@@ -3,8 +3,11 @@ import {redirect} from 'next/navigation';
 
 export const register = async (request) => {
   const org_code = request.nextUrl.searchParams.get('org_code');
+  const post_login_redirect_url = request.nextUrl.searchParams.get(
+    'post_login_redirect_url'
+  );
 
-  const options = {org_code};
+  const options = {org_code, post_login_redirect_url};
 
   const authUrl = prepareForRedirect(options, 'register');
 
