7230211874251246bc29169269a1bde8b2ed1e15
Peter Phanouvong <peterphanouvong@gmail.com>
2023-09-27 14:21:52 +1000

    Login + register link search params

--- src/components/RegisterLink.js
+++ src/components/RegisterLink.js
@@ -2,10 +2,26 @@ import React from 'react';
 
 import {config} from '../config/index';
 
-export function RegisterLink({children, orgCode, ...props}) {
+export function RegisterLink({
+  children,
+  orgCode,
+  postLoginRedirectURL,
+  ...props
+}) {
+  const params = new URLSearchParams(
+    JSON.parse(
+      JSON.stringify({
+        org_code: orgCode,
+        post_login_redirect_url: postLoginRedirectURL
+      })
+    )
+  );
+
   return (
     <a
-      href={`${config.apiPath}/register${orgCode ? `?org_code=${orgCode}` : ''}`}
+      href={`${config.apiPath}/register${
+        params ? `?${params.toString()}` : ''
+      }`}
       {...props}
     >
       {children}
