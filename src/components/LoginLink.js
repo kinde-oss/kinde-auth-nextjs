b2e9f7ac3d2871bf277d749a5099e94225de15e0
Peter Phanouvong <peterphanouvong@gmail.com>
2023-09-28 11:30:44 +1000

    query params

--- src/components/LoginLink.js
+++ src/components/LoginLink.js
@@ -3,14 +3,10 @@ import React from 'react';
 import {config} from '../config/index';
 
 export function LoginLink({children, postLoginRedirectURL, orgCode, ...props}) {
-  const params = new URLSearchParams(
-    JSON.parse(
-      JSON.stringify({
-        org_code: orgCode,
-        post_login_redirect_url: postLoginRedirectURL
-      })
-    )
-  );
+  let params = new URLSearchParams();
+  orgCode && params.append('org_code', orgCode);
+  postLoginRedirectURL &&
+    params.append('post_login_redirect_url', postLoginRedirectURL);
   return (
     <a
       href={`${config.apiPath}/login${params ? `?${params.toString()}` : ''}`}
