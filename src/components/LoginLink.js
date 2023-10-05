eed232d6a8171e75fe505de02220027fe6533ec0
Peter Phanouvong <peterphanouvong@gmail.com>
2023-10-05 11:07:28 +1100

    link types

--- src/components/LoginLink.js
+++ src/components/LoginLink.js
@@ -2,6 +2,18 @@ import React from 'react';
 
 import {config} from '../config/index';
 
+/**
+ * @typedef {Object} PropsType
+ * @prop {React.ReactNode} children
+ * @prop {string} [orgCode]
+ * @prop {string} [postLoginRedirectURL]
+ *
+ * @typedef {PropsType & React.AnchorHTMLAttributes<HTMLAnchorElement>} Props
+ */
+
+/**
+ * @param {Props} props
+ */
 export function LoginLink({children, orgCode, ...props}) {
   return (
     <a
--- src/components/RegisterLink.js
+++ src/components/RegisterLink.js
@@ -1,11 +1,24 @@
 import React from 'react';
 
 import {config} from '../config/index';
+/**
+ * @typedef {Object} PropsType
+ * @prop {React.ReactNode} children
+ * @prop {string} [orgCode]
+ * @prop {string} [postLoginRedirectURL]
+ *
+ * @typedef {PropsType & React.AnchorHTMLAttributes<HTMLAnchorElement>} Props
+ */
 
+/**
+ * @param {Props} props
+ */
 export function RegisterLink({children, orgCode, ...props}) {
   return (
     <a
-      href={`${config.apiPath}/register${orgCode ? `?org_code=${orgCode}` : ''}`}
+      href={`${config.apiPath}/register${
+        orgCode ? `?org_code=${orgCode}` : ''
+      }`}
       {...props}
     >
       {children}
