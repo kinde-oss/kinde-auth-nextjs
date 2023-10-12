5c311263a54fb118e1705dfebadd95b2a4646d91
Peter Phanouvong <peterphanouvong@gmail.com>
2023-10-12 12:43:14 +1100

    components + post login redirect

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,5 @@
 export {getKindeServerSession} from '../session/index';
 export {authMiddleware, withAuth} from '../authMiddleware/authMiddleware';
 
-export {RegisterLink} from '../components/RegisterLink';
-export {LoginLink} from '../components/LoginLink';
-export {LogoutLink} from '../components/LogoutLink';
-export {CreateOrgLink} from '../components/CreateOrgLink';
 export {createKindeManagementAPIClient} from '../api-client';
 export {default as handleAuth} from '../handlers/auth';
