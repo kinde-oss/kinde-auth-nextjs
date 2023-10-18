815cfc359da78004094423dbecf65ee6dfee14a8
Dave Berner <davidajberner@gmail.com>
2023-10-18 20:59:27 +1100

    feat: make sure always a single slash between domain and path

---
+++ src/utils/generateCallbackUrl.js
@@ -0,0 +1,7 @@
+const generateCallbackUrl = (base, path) => {
+  const siteUrl = base.endsWith('/') ? base.slice(0, -1) : base;
+  const callbackPath = path.startsWith('/') ? path.substr(1) : path;
+  return `${siteUrl}/${callbackPath}`;
+};
+
+export {generateCallbackUrl};
