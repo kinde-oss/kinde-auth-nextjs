02d07aa25f4f00465b910b63b97afdbaf7982310
Peter Phanouvong <peterphanouvong@gmail.com>
2023-10-30 23:22:36 +1100

    chore: export components from server and index to avoid breaking changes

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,10 @@
 export {getKindeServerSession} from '../session/index';
 export {authMiddleware, withAuth} from '../authMiddleware/authMiddleware';
-
+export {
+  LoginLink,
+  CreateOrgLink,
+  LogoutLink,
+  RegisterLink
+} from '../components/index';
 export {createKindeManagementAPIClient} from '../api-client';
 export {default as handleAuth} from '../handlers/auth';
