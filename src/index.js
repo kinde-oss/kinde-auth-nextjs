ec1835bd8d274f3f02ae57838c370501e2c9b63e
Peter Phanouvong <peterphanouvong@gmail.com>
2023-10-30 23:22:36 +1100

    chore: export components from server and index to avoid breaking changes

--- src/index.js
+++ src/index.js
@@ -4,3 +4,9 @@ export {
   useKindeBrowserClient
 } from './frontend/index';
 export {isTokenValid} from './utils/pageRouter/isTokenValid';
+export {
+  LoginLink,
+  CreateOrgLink,
+  LogoutLink,
+  RegisterLink
+} from './components/index';
