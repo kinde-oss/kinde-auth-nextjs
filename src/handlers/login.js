b26b9b680c01e07c2f385c3ba632939b69fa6a05
Peter Phanouvong <peterphanouvong@gmail.com>
2023-10-30 11:43:40 +1100

    post login redirect

--- src/handlers/login.js
+++ src/handlers/login.js
@@ -3,5 +3,16 @@ export const login = async (routerClient) => {
     routerClient.sessionManager
   );
 
+  const postLoginRedirectURL = routerClient.getSearchParam(
+    'post_login_redirect_url'
+  );
+
+  if (postLoginRedirectURL) {
+    routerClient.sessionManager.setSessionItem(
+      'post_login_redirect_url',
+      postLoginRedirectURL
+    );
+  }
+
   routerClient.redirect(authUrl);
 };
