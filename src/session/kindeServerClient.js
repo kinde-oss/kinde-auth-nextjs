cdffc62069293fa647f30d0f431a5147a6757180
Peter Phanouvong <peterphanouvong@gmail.com>
2023-11-02 21:26:54 +1100

    types: client update

--- src/session/kindeServerClient.js
+++ src/session/kindeServerClient.js
@@ -2,6 +2,7 @@ import {createKindeServerClient} from '@kinde-oss/kinde-typescript-sdk';
 import {config} from '../config/index';
 
 /** @type {import('../../types').KindeClient} */
+// @ts-ignore
 export const kindeClient = createKindeServerClient(
   config.grantType,
   config.clientOptions
