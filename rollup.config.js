c6c83a920702859ce3c1f32781b5f73a0fc604da
Peter Phanouvong <peterphanouvong@gmail.com>
2023-11-08 18:35:09 +1100

    fix: types

--- rollup.config.js
+++ rollup.config.js
@@ -8,8 +8,23 @@ export default [
   serverConfig,
   componentsConfig,
   {
+    input: './types.d.ts',
+    output: [{file: 'dist/types.d.ts', format: 'es'}],
+    plugins: [dts()]
+  },
+  {
     input: './dist/types/index.d.ts',
     output: [{file: 'dist/index.d.ts', format: 'es'}],
+    plugins: [dts()]
+  },
+  {
+    input: './dist/types/server/index.d.ts',
+    output: [{file: 'dist/server/index.d.ts', format: 'es'}],
+    plugins: [dts()]
+  },
+  {
+    input: './dist/types/components/index.d.ts',
+    output: [{file: 'dist/components/index.d.ts', format: 'es'}],
     plugins: [dts()]
   }
 ];
