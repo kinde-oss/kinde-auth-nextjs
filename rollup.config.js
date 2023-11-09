503df6098d2ab87a8503bb22d1d908008ef44f55
Peter Phanouvong <peterphanouvong@gmail.com>
2023-11-09 13:35:06 +1100

    fix: type files bundles with dts

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import packageJson from './package.json' assert {type: 'json'};
 import babel from '@rollup/plugin-babel';
 import terser from '@rollup/plugin-terser';
 import {nodeResolve} from '@rollup/plugin-node-resolve';
+import dts from 'rollup-plugin-dts';
 
 export default [
   {
@@ -77,5 +78,48 @@ export default [
         exports: 'named'
       }
     ]
+  },
+  {
+    plugins: [babel({babelHelpers: 'bundled'}), terser()],
+    input: 'src/middleware/index.js',
+    output: [
+      {
+        file: 'dist/middleware/index.js',
+        format: 'esm',
+        sourcemap: true,
+        exports: 'named'
+      },
+      {
+        file: 'dist/middleware/cjs/index.js',
+        format: 'cjs',
+        sourcemap: true,
+        exports: 'named'
+      }
+    ]
+  },
+  {
+    input: './types.d.ts',
+    output: [{file: 'dist/types.d.ts', format: 'esm'}],
+    plugins: [dts()]
+  },
+  {
+    input: './dist/types/index.d.ts',
+    output: [{file: 'dist/index.d.ts', format: 'esm'}],
+    plugins: [dts()]
+  },
+  {
+    input: './dist/types/server/index.d.ts',
+    output: [{file: 'dist/server/index.d.ts', format: 'esm'}],
+    plugins: [dts()]
+  },
+  {
+    input: './dist/types/components/index.d.ts',
+    output: [{file: 'dist/components/index.d.ts', format: 'esm'}],
+    plugins: [dts()]
+  },
+  {
+    input: './dist/types/middleware/index.d.ts',
+    output: [{file: 'dist/middleware/index.d.ts', format: 'esm'}],
+    plugins: [dts()]
   }
 ];
