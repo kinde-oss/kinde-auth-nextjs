037e1d6958749b6840931cde1d762dde149a78fe
Peter Phanouvong <peterphanouvong@gmail.com>
2023-11-09 12:14:01 +1100

    chore: simplify rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,26 @@
-import clientConfig from './clientConfig/rollup.config.js';
-import serverConfig from './serverConfig/rollup.config.js';
-import componentsConfig from './componentsConfig/rollup.config.js';
-import {dts} from 'rollup-plugin-dts';
+import packageJson from './package.json' assert {type: 'json'};
+import babel from '@rollup/plugin-babel';
+import terser from '@rollup/plugin-terser';
+import {nodeResolve} from '@rollup/plugin-node-resolve';
 
 export default [
-  clientConfig,
-  serverConfig,
-  componentsConfig,
   {
-    input: './types.d.ts',
-    output: [{file: 'dist/types.d.ts', format: 'esm'}],
-    plugins: [dts()]
-  },
-  {
-    input: './dist/types/index.d.ts',
-    output: [{file: 'dist/index.d.ts', format: 'esm'}],
-    plugins: [dts()]
-  },
-  {
-    input: './dist/types/server/index.d.ts',
-    output: [{file: 'dist/server/index.d.ts', format: 'esm'}],
-    plugins: [dts()]
-  },
-  {
-    input: './dist/types/components/index.d.ts',
-    output: [{file: 'dist/components/index.d.ts', format: 'esm'}],
-    plugins: [dts()]
+    plugins: [babel({babelHelpers: 'bundled'}), terser(), nodeResolve()],
+    input: 'src/index.js',
+    output: [
+      {
+        file: packageJson.module,
+        format: 'esm',
+        sourcemap: true,
+        exports: 'named'
+      },
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: true,
+        exports: 'named'
+      }
+    ],
+    external: ['react', 'react-dom']
   }
 ];
