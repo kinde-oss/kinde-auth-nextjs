17239525f50ccd3d39281fed58c9da2c535b0874
Peter Phanouvong <peterphanouvong@gmail.com>
2023-11-09 13:06:16 +1100

    fix: pacakge exports & middleware

--- rollup.config.js
+++ rollup.config.js
@@ -22,5 +22,60 @@ export default [
       }
     ],
     external: ['react', 'react-dom']
+  },
+  {
+    plugins: [babel({babelHelpers: 'bundled'}), terser()],
+    input: 'src/server/index.js',
+    output: [
+      {
+        file: 'dist/server/index.js',
+        format: 'esm',
+        sourcemap: true,
+        exports: 'named'
+      },
+      {
+        file: 'dist/server/cjs/index.js',
+        format: 'cjs',
+        sourcemap: true,
+        exports: 'named'
+      }
+    ]
+  },
+  {
+    plugins: [babel({babelHelpers: 'bundled'}), terser(), nodeResolve()],
+    input: 'src/components/index.js',
+    output: [
+      {
+        file: 'dist/components/index.js',
+        format: 'esm',
+        sourcemap: true,
+        exports: 'named'
+      },
+      {
+        file: 'dist/components/cjs/index.js',
+        format: 'cjs',
+        sourcemap: true,
+        exports: 'named'
+      }
+    ],
+    external: ['react', 'react-dom']
+  },
+  {
+    plugins: [babel({babelHelpers: 'bundled'}), terser()],
+    input: 'src/middleware/index.js',
+    output: [
+      {
+        file: 'dist/middleware/index.js',
+        format: 'esm',
+        sourcemap: true,
+        exports: 'named'
+      },
+      {
+        file: 'dist/middleware/cjs/index.js',
+        format: 'cjs',
+        sourcemap: true,
+        exports: 'named'
+      }
+    ]
   }
 ];
