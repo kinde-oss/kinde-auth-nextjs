923ef1fb12bf96c7bd560ec42539c1961184d83b
Peter Phanouvong <peterphanouvong@gmail.com>
2023-11-13 14:39:00 +1100

    remove unused files

--- src/utils/randomString.js
+++
@@ -1,7 +0,0 @@
-const {getRandomValues} = require("uncrypto");
-
-export const randomString = () => {
-    const buffer = new Uint8Array(28);
-    getRandomValues(buffer);
-    return Array.from(buffer).map((n) => n.toString(16).padStart(2, "0")).join("");
-}
--- src/utils/setupChallenge.js
+++
@@ -1,9 +0,0 @@
-import pkceChallenge from './pkceChallenge';
-import {randomString} from './randomString';
-
-export const setupChallenge = (req, res) => {
-  const state = randomString();
-  const {code_challenge, code_verifier} = pkceChallenge();
-
-  return {state, code_challenge, code_verifier};
-};
