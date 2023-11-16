85614677b9fc180a3ec4d209cc8cabc95e941441
Peter Phanouvong <peterphanouvong@gmail.com>
2023-11-17 10:54:16 +1100

    fix: NextResponse not imported

--- src/routerClients/AppRouterClient.js
+++ src/routerClients/AppRouterClient.js
@@ -1,7 +1,7 @@
 import {createKindeServerClient} from '@kinde-oss/kinde-typescript-sdk';
 import {cookies} from 'next/headers';
 import {redirect} from 'next/navigation';
-import {NextRequest} from 'next/server';
+import {NextRequest, NextResponse} from 'next/server';
 import {config} from '../config/index';
 import {appRouterSessionManager} from '../session/sessionManager';
 import RouterClient from './RouterClient';
