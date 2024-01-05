a162335f3c853492523d064ca514efae3adeedde
Peter Phanouvong <peterphanouvong@gmail.com>
2024-01-05 11:57:03 +1100

    feat: raw access token and raw id token available via helpers

---
+++ src/session/getIdTokenRaw.js
@@ -0,0 +1,18 @@
+import {sessionManager} from './sessionManager';
+
+/**
+ * @callback getIdTokenRaw
+ * @returns {Promise<string>}
+ */
+
+/**
+ *
+ * @param {import('next').NextApiRequest} [req]
+ * @param {import('next').NextApiResponse} [res]
+ * @returns {getIdTokenRaw}
+ */
+
+// @ts-ignore
+export const getIdTokenRawFactory = (req, res) => async () => {
+  return await sessionManager(req, res).getSessionItem('id_token');
+};
