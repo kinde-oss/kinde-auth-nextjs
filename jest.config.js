47eb039079b8639df9df8259ae245f13c1725a7c
Peter Phanouvong <peterphanouvong@gmail.com>
2024-02-05 12:24:34 +1100

    test: apiPath browser client

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest'
+  },
+  moduleNameMapper: {
+    uncrypto: 'uncrypto'
+  }
+};
