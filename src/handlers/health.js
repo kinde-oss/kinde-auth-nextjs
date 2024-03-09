c72c807f08920751507577afbb6771d1d445734c
Daniel Rivers <me@danielrivers.com>
2024-03-09 22:28:02 +0000

    deps: update TS SDK and use validateClientSecret

--- src/handlers/health.js
+++ src/handlers/health.js
@@ -1,6 +1,6 @@
-import RouterClient from '../routerClients/RouterClient';
 import { config } from '../config/index'
 import { NextResponse } from "next/server";
+import { validateClientSecret } from '@kinde-oss/kinde-typescript-sdk';
 
 /**
  * 
@@ -11,10 +11,10 @@ export const health = async () => {
     apiPath: config.apiPath,
     redirectURL: config.clientOptions.redirectURL,
     postLoginRedirectURL: config.postLoginRedirectURL,
-    issuerURL:  config.issuerURL,
-    clientID:  config.clientID,
-    clientSecret:  config.clientSecret.match('[a-z0-9]{32}') ? 'Set correctly' : 'Not set correctly',
-    postLogoutRedirectURL:  config.postLogoutRedirectURL,
+    issuerURL: config.issuerURL,
+    clientID: config.clientID,
+    clientSecret: validateClientSecret(config.clientSecret) ? 'Set correctly' : 'Not set correctly',
+    postLogoutRedirectURL: config.postLogoutRedirectURL,
     audience: config.audience,
     cookieDomain: config.cookieDomain,
     logoutRedirectURL: config.clientOptions.logoutRedirectURL
