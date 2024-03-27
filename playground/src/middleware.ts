e9774a0615a21e1de7acbe53de88660df46412d4
Daniel Rivers <me@danielrivers.com>
2024-03-27 22:24:59 +0000

    feat: add playground

---
+++ playground/src/middleware.ts
@@ -0,0 +1,12 @@
+import {
+  authMiddleware,
+  withAuth,
+} from "@kinde-oss/kinde-auth-nextjs/middleware";
+
+export default function middleware(req: Request) {
+  return withAuth(req);
+}
+
+export const config = {
+  matcher: ["/dashboard"],
+};
