36649d8f41799c98a7e595ffc03ed674879b4169
Peter Phanouvong <peterphanouvong@gmail.com>
2024-05-02 11:44:25 +1000

    fix: v1 & v2 sync

--- src/handlers/pageRouter/logout.js
+++ src/handlers/pageRouter/logout.js
@@ -3,17 +3,43 @@ import {config} from '../../config/index';
 var cookie = require('cookie');
 
 export const logout = async (req, res) => {
-  res.setHeader(
-    'Set-Cookie',
+  res.setHeader('Set-Cookie', [
     cookie.serialize('kinde_token', null, {
       httpOnly: true,
       expires: new Date(0),
       sameSite: 'lax',
       secure: config.redirectURL.substring(0, 6) == 'https:',
-      path: '/',
-      domain: config.cookieDomain ? config.cookieDomain : undefined
+      path: '/'
+    }),
+    cookie.serialize('kinde_token', null, {
+      httpOnly: true,
+      expires: new Date(0),
+      sameSite: 'lax',
+      secure: config.redirectURL.substring(0, 6) == 'https:',
+      path: '/'
+    }),
+    cookie.serialize('access_token', null, {
+      httpOnly: true,
+      expires: new Date(0),
+      sameSite: 'lax',
+      secure: config.redirectURL.substring(0, 6) == 'https:',
+      path: '/'
+    }),
+    cookie.serialize('id_token', null, {
+      httpOnly: true,
+      expires: new Date(0),
+      sameSite: 'lax',
+      secure: config.redirectURL.substring(0, 6) == 'https:',
+      path: '/'
+    }),
+    cookie.serialize('refresh_token', null, {
+      httpOnly: true,
+      expires: new Date(0),
+      sameSite: 'lax',
+      secure: config.redirectURL.substring(0, 6) == 'https:',
+      path: '/'
     })
-  );
+  ]);
 
   const logoutURL = new URL(config.issuerURL + config.issuerRoutes.logout);
 
