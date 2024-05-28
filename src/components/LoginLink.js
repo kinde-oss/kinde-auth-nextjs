92927e089fff6de3116faf76643b568df5ab276c
Daniel Rivers <me@danielrivers.com>
2024-05-28 19:08:09 +0100

    fix: use host/environment variable if relative postLoginRedirectURL supplied

--- src/components/LoginLink.js
+++ src/components/LoginLink.js
@@ -24,16 +24,22 @@ export function LoginLink({
   let params = new URLSearchParams();
   let paramsObj = {};
   if (orgCode != null) paramsObj.org_code = orgCode;
-  if (postLoginRedirectURL != null)
+  if (postLoginRedirectURL != null) {
+    if (postLoginRedirectURL.startsWith('/')) {
+      const host = typeof window !== 'undefined' ? window.location.origin : process.env.KINDE_SITE_URL;
+      postLoginRedirectURL = `${host}${postLoginRedirectURL}`;
+    }
     paramsObj.post_login_redirect_url = postLoginRedirectURL;
+  }
 
   paramsObj = {...authUrlParams, ...paramsObj};
 
   for (const key in paramsObj) params.append(key, paramsObj[key]);
 
+  const authUrl = `${config.apiPath}/login${params ? `?${params.toString()}` : ''}`;
   return (
     <a
-      href={`${config.apiPath}/login${params ? `?${params.toString()}` : ''}`}
+      href={authUrl}
       {...props}
     >
       {children}
