58ff9e5bf948d897acb091e964353b9028fc2aa9
Peter Phanouvong <peterphanouvong@gmail.com>
2024-09-03 14:21:54 +1000

    chore: sanitise redirect url

--- src/handlers/refresh.ts
+++ src/handlers/refresh.ts
@@ -2,14 +2,29 @@ import {NextResponse} from 'next/server';
 import {config} from '../config/index';
 import RouterClient from '../routerClients/RouterClient';
 
+const handleSuccess = (routerClient: RouterClient, redirectURL: string) => {
+  const returnTo = validateAndSanitizeReturnTo(
+    routerClient.req.nextUrl.searchParams.get('returnTo')
+  );
+  return NextResponse.redirect(redirectURL + returnTo);
+};
+
+const handleError = (error: Error) => {
+  return NextResponse.json({error: 'Token refresh failed'}, {status: 401});
+};
+
+const validateAndSanitizeReturnTo = (returnTo: string | null) => {
+  // Implement validation and sanitization logic here
+  // ...
+  const sanitizedReturnTo = returnTo ? `?returnTo=${returnTo}` : '';
+  return sanitizedReturnTo;
+};
+
 export const refresh = async (routerClient: RouterClient) => {
   try {
     await routerClient.kindeClient.refreshTokens(routerClient.sessionManager);
-    return NextResponse.redirect(
-      config.redirectURL + routerClient.req.nextUrl.searchParams.get('returnTo')
-    );
+    return handleSuccess(routerClient, config.redirectURL);
   } catch (error) {
-    console.error('refresh', error);
-    return routerClient.json({error: error.message}, {status: 500});
+    return handleError(error);
   }
 };
