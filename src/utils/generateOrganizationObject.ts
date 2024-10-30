55bae1a4a481622df6375659dfdf21877e985f8a
Peter Phanouvong <peterphanouvong@gmail.com>
2024-10-30 14:05:06 +1100

    chore: refactor for cleaner and safer code

--- src/utils/generateOrganizationObject.ts
+++ src/utils/generateOrganizationObject.ts
@@ -1,5 +1,23 @@
 import {KindeAccessToken, KindeIdToken} from '../../types';
 
+type OrgPropertyKey =
+  | 'city'
+  | 'industry'
+  | 'postcode'
+  | 'state_region'
+  | 'street_address'
+  | 'street_address_2';
+
+const getOrgProperty = (
+  key: OrgPropertyKey,
+  idToken: KindeIdToken,
+  accessToken: KindeAccessToken
+): string | undefined => {
+  const idValue = idToken.organization_properties?.[`kp_org_${key}`]?.v;
+  const accessValue = accessToken.organization_properties?.[`kp_org_${key}`]?.v;
+  return idValue || accessValue;
+};
+
 export const generateOrganizationObject = (
   idToken: KindeIdToken,
   accessToken: KindeAccessToken
@@ -8,24 +26,12 @@ export const generateOrganizationObject = (
     orgCode: accessToken.org_code,
     orgName: accessToken.org_name,
     properties: {
-      city:
-        idToken.organization_properties?.kp_org_city?.v ||
-        accessToken.organization_properties?.kp_org_city?.v,
-      industry:
-        idToken.organization_properties?.kp_org_industry?.v ||
-        accessToken.organization_properties?.kp_org_industry?.v,
-      postcode:
-        idToken.organization_properties?.kp_org_postcode?.v ||
-        accessToken.organization_properties?.kp_org_postcode?.v,
-      state_region:
-        idToken.organization_properties?.kp_org_state_region?.v ||
-        accessToken.organization_properties?.kp_org_state_region?.v,
-      street_address:
-        idToken.organization_properties?.kp_org_street_address?.v ||
-        accessToken.organization_properties?.kp_org_street_address?.v,
-      street_address_2:
-        idToken.organization_properties?.kp_org_street_address_2?.v ||
-        accessToken.organization_properties?.kp_org_street_address_2?.v
+      city: getOrgProperty('city', idToken, accessToken),
+      industry: getOrgProperty('industry', idToken, accessToken),
+      postcode: getOrgProperty('postcode', idToken, accessToken),
+      state_region: getOrgProperty('state_region', idToken, accessToken),
+      street_address: getOrgProperty('street_address', idToken, accessToken),
+      street_address_2: getOrgProperty('street_address_2', idToken, accessToken)
     }
   };
 };
