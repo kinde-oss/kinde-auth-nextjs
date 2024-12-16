8a5a13c8be8159cc650dc3745e2fecc5b44cfc7e
Bailey Eaton <xYoshify@gmail.com>
2024-12-17 02:20:58 +1000

    feat(auto-refresh): add a token expiry check to isAuthenticatedFactory
    Prevents edge cases where users accessing `getKindeServerSession` on an RSC that isn't protected by their middleware, after their tokens have expired, will be operating with stale tokens.
    
    If the token is expired here, we'll redirect to login.
    
    Note: this would probably be better in the `getKindeServerSession` root, but we'd need to make it async. Maybe something to consider down the track.

--- src/session/isAuthenticated.js
+++ src/session/isAuthenticated.js
@@ -1,5 +1,8 @@
 import { getUserFactory } from "./getUser";
 import { getAccessToken } from "../utils/getAccessToken";
+import { isTokenExpired } from "../utils/jwt/validation";
+import { redirect } from "next/navigation";
+
 
 /**
  *
@@ -9,7 +12,11 @@ import { getAccessToken } from "../utils/getAccessToken";
  */
 export const isAuthenticatedFactory = (req, res) => async () => {
   const token = await getAccessToken(req, res);
-  console.log('isAuthenticatedFactory token', token)
+  console.log('isAuthenticatedFactory: token', token)
+  if(isTokenExpired(token)) {
+    console.log('isAuthenticatedFactory: token is expired, redirecting to login')
+    redirect('/api/auth/login')
+  }
   const user = await getUserFactory(req, res)();
   return token && Boolean(user);
 };
