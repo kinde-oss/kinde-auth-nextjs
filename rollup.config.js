9a94f2684d6df5f8840ff83b28d847728aca6939
Bailey Eaton <xYoshify@gmail.com>
2025-02-14 23:09:00 +1000

    fix: rollup typo

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import clientConfig from "./rollupConfig/clientConfig.config.js";
 import serverConfig from "./rollupConfig/serverConfig.config.js";
 import componentConfig from "./rollupConfig/componentConfig.config.js";
-import middlewareConfig from "./rollupConfig/middlewearConfig.config.js";
+import middlewareConfig from "./rollupConfig/middlewareConfig.config.js";
 
 export default [
     clientConfig,
