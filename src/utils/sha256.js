f4ea7e306d2e66b8319db732b6a5331dc5bc9143
Daniel Rivers <me@danielrivers.com>
2025-04-17 01:08:43 +0100

    refactor: utilise js-utils utilities

--- src/utils/sha256.js
+++
@@ -1,8 +0,0 @@
-import { subtle } from "uncrypto";
-// Calculate the SHA256 hash of the input text.
-// Returns a promise that resolves to an ArrayBuffer
-export function sha256(plain) {
-  const encoder = new TextEncoder();
-  const data = encoder.encode(plain);
-  return subtle.digest("SHA-256", data);
-}
