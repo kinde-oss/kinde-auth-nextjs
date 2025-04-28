e26f1e6f669fc98c29ffc62e8aaa2b4c1565bbf4
Bailey Eaton <xYoshify@gmail.com>
2025-04-29 00:40:31 +1000

    chore: enhance return type of `getStandardCookieOptions`

--- src/utils/cookies/getStandardCookieOptions.ts
+++ src/utils/cookies/getStandardCookieOptions.ts
@@ -2,7 +2,7 @@ import { ResponseCookie } from 'next/dist/compiled/@edge-runtime/cookies';
 import { config } from 'src/config';
 import { GLOBAL_COOKIE_OPTIONS, TWENTY_NINE_DAYS } from 'src/utils/constants';
 
-export const getStandardCookieOptions = (): Partial<ResponseCookie> => {
+export const getStandardCookieOptions = (): Omit<ResponseCookie, 'name' | 'value'> => {
   return {
     maxAge: TWENTY_NINE_DAYS,
     domain: config.cookieDomain ? config.cookieDomain : undefined,
