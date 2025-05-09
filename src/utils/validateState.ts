ee96e04a636976fafcc8ad00acc263f1160460a4
Daniel Rivers <me@danielrivers.com>
2025-05-09 12:06:20 +0100

    fix: update state check regex

--- src/utils/validateState.ts
+++ src/utils/validateState.ts
@@ -1,5 +1,5 @@
 export default function validateState(passedState: string) {
-  if (!/^[a-zA-Z0-9-_]+$/.test(passedState)) {
+  if (!/^[a-zA-Z0-9+/=_-]+$/.test(passedState)) {
     return false;
   }
   return true;
