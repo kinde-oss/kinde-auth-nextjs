1bd67ef0f25bbd7bc2cbfb7e7f086efbe5d73446
Sudipta Pradhan <sudiptapradhan58271@gmail.com>
2025-07-10 10:57:47 +0530

    linting fixes

--- src/authMiddleware/authMiddleware.ts
+++ src/authMiddleware/authMiddleware.ts
@@ -59,7 +59,7 @@ const handleMiddleware = async (req, options, onSuccess) => {
   const isPublicPath = isPublicPathMatch(
     pathname,
     publicPaths,
-    config.isDebugMode
+    config.isDebugMode,
   );
 
   // getAccessToken will validate the token
@@ -71,11 +71,11 @@ const handleMiddleware = async (req, options, onSuccess) => {
   if ((!kindeAccessToken || !kindeIdToken) && !isPublicPath) {
     if (config.isDebugMode) {
       console.log(
-        "authMiddleware: no access or id token, redirecting to login"
+        "authMiddleware: no access or id token, redirecting to login",
       );
     }
     return NextResponse.redirect(
-      new URL(loginRedirectUrl, options?.redirectURLBase || config.redirectURL)
+      new URL(loginRedirectUrl, options?.redirectURLBase || config.redirectURL),
     );
   }
 
@@ -97,8 +97,8 @@ const handleMiddleware = async (req, options, onSuccess) => {
         return NextResponse.redirect(
           new URL(
             loginRedirectUrl,
-            options?.redirectURLBase || config.redirectURL
-          )
+            options?.redirectURLBase || config.redirectURL,
+          ),
         );
       }
       return undefined;
@@ -112,7 +112,7 @@ const handleMiddleware = async (req, options, onSuccess) => {
         console.log(
           "authMiddleware: tokens refreshed",
           !!refreshResponse.access_token,
-          !!refreshResponse.id_token
+          !!refreshResponse.id_token,
         );
       }
     } catch (error) {
@@ -125,7 +125,7 @@ const handleMiddleware = async (req, options, onSuccess) => {
       // we need to set the cookie on the response here
       const splitAccessTokenCookies = getSplitCookies(
         "access_token",
-        refreshResponse.access_token
+        refreshResponse.access_token,
       );
       splitAccessTokenCookies.forEach((cookie) => {
         resp.cookies.set(cookie.name, cookie.value, cookie.options);
@@ -133,7 +133,7 @@ const handleMiddleware = async (req, options, onSuccess) => {
 
       const splitIdTokenCookies = getSplitCookies(
         "id_token",
-        refreshResponse.id_token
+        refreshResponse.id_token,
       );
       splitIdTokenCookies.forEach((cookie) => {
         resp.cookies.set(cookie.name, cookie.value, cookie.options);
@@ -142,7 +142,7 @@ const handleMiddleware = async (req, options, onSuccess) => {
       resp.cookies.set(
         "refresh_token",
         refreshResponse.refresh_token,
-        getStandardCookieOptions()
+        getStandardCookieOptions(),
       );
 
       // copy the cookies from the response to the request
@@ -156,7 +156,7 @@ const handleMiddleware = async (req, options, onSuccess) => {
       }
     } catch (error) {
       const result = sendResult(
-        "authMiddleware: error settings new token in cookie"
+        "authMiddleware: error settings new token in cookie",
       );
       if (result) return result;
     }
@@ -176,11 +176,11 @@ const handleMiddleware = async (req, options, onSuccess) => {
   } catch (error) {
     if (config.isDebugMode) {
       console.error(
-        "authMiddleware: access token decode failed, redirecting to login"
+        "authMiddleware: access token decode failed, redirecting to login",
       );
     }
     return NextResponse.redirect(
-      new URL(loginRedirectUrl, options?.redirectURLBase || config.redirectURL)
+      new URL(loginRedirectUrl, options?.redirectURLBase || config.redirectURL),
     );
   }
 
@@ -189,11 +189,11 @@ const handleMiddleware = async (req, options, onSuccess) => {
   } catch (error) {
     if (config.isDebugMode) {
       console.error(
-        "authMiddleware: id token decode failed, redirecting to login"
+        "authMiddleware: id token decode failed, redirecting to login",
       );
     }
     return NextResponse.redirect(
-      new URL(loginRedirectUrl, options?.redirectURLBase || config.redirectURL)
+      new URL(loginRedirectUrl, options?.redirectURLBase || config.redirectURL),
     );
   }
 
@@ -220,7 +220,7 @@ const handleMiddleware = async (req, options, onSuccess) => {
     if (callbackResult instanceof NextResponse) {
       if (config.isDebugMode) {
         console.log(
-          "authMiddleware: onSuccess callback returned a response, copying our cookies to it"
+          "authMiddleware: onSuccess callback returned a response, copying our cookies to it",
         );
       }
       // Copy our cookies to their response
@@ -241,7 +241,7 @@ const handleMiddleware = async (req, options, onSuccess) => {
     // If they didn't return a response, return our response with the refreshed tokens
     if (config.isDebugMode) {
       console.log(
-        "authMiddleware: onSuccess callback did not return a response, returning our response"
+        "authMiddleware: onSuccess callback did not return a response, returning our response",
       );
     }
 
@@ -251,7 +251,7 @@ const handleMiddleware = async (req, options, onSuccess) => {
   if (customValidationValid) {
     if (config.isDebugMode) {
       console.log(
-        "authMiddleware: customValidationValid is true, returning response"
+        "authMiddleware: customValidationValid is true, returning response",
       );
     }
     return resp;
@@ -262,7 +262,7 @@ const handleMiddleware = async (req, options, onSuccess) => {
   }
 
   return NextResponse.redirect(
-    new URL(loginRedirectUrl, options?.redirectURLBase || config.redirectURL)
+    new URL(loginRedirectUrl, options?.redirectURLBase || config.redirectURL),
   );
 };
 
