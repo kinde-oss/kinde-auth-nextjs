9fc866d111ece962a83446fa399308555d205836
Bailey Eaton <xYoshify@gmail.com>
2025-10-03 07:30:17 +1000

    chore: add strict return type signature

--- src/frontend/hooks/public/use-kinde-browser-client.ts
+++ src/frontend/hooks/public/use-kinde-browser-client.ts
@@ -3,19 +3,17 @@ import { useContext } from "react";
 import { useProvidedKindeAuth } from "./use-provided-kinde-auth";
 import { useProviderlessKindeAuth } from "./use-providerless-kinde-auth";
 import { KindeContext } from "@kinde-oss/kinde-auth-react";
+import { KindeState } from "../../../types";
 
-export const useKindeBrowserClient = () => {
+export const useKindeBrowserClient = (): KindeState => {
   const context = useContext(KindeContext);
 
   // If KindeContext provider exists, use the provided auth
   // Otherwise, use the providerless implementation
   if (context) {
-    console.log("Using provided KindeAuth");
     // eslint-disable-next-line react-hooks/rules-of-hooks
     return useProvidedKindeAuth();
   }
-
-  console.log("Using providerless KindeAuth");
   // eslint-disable-next-line react-hooks/rules-of-hooks
   return useProviderlessKindeAuth();
 };
