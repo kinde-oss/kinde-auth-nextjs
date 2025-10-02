a5c49544153db288a934b98b9af30cd62cd3285d
Bailey Eaton <xYoshify@gmail.com>
2025-10-03 02:37:48 +1000

    feat: `useKindeBrowserClient` now routes to provided or providerless hooks base on context

---
+++ src/frontend/hooks/public/use-kinde-browser-client.ts
@@ -0,0 +1,21 @@
+'use client';
+import { useContext } from "react";
+import { useProvidedKindeAuth } from "./use-provided-kinde-auth";
+import { useProviderlessKindeAuth } from "./use-providerless-kinde-auth";
+import { KindeContext } from "@kinde-oss/kinde-auth-react";
+
+export const useKindeBrowserClient = () => {
+  const context = useContext(KindeContext);
+  
+  // If KindeContext provider exists, use the provided auth
+  // Otherwise, use the providerless implementation
+  if (context) {
+    console.log("Using provided KindeAuth");
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    return useProvidedKindeAuth();
+  }
+  
+  console.log("Using providerless KindeAuth");
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  return useProviderlessKindeAuth();
+};
